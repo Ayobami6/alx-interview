a80d89977324601b329d131d0b8bab12419c551f
Ayobami <ayobamidele006@gmail.com>
2023-07-31 17:10:12 +0100

    document code&update readme

--- lockboxex.py
+++ lockboxex.py
@@ -1,33 +1,35 @@
-#!/usr/bin/python3
-""" Lockboxes interview coding challenge """
-
 from typing import List
 
 
 def canUnlockAll(boxes: List[list]) -> bool:
-    """ checks if the all boxes can be unlocked
-
-    Args:
-        boxes (List[list]): List of boxes
-
-    Returns:
-        bool: True if all boxes can be unlocked else fals
-    """
     unlocked_boxes = {}
     try:
         empty_index = boxes.index([])
         boxes[empty_index] = [0]
     except Exception:
         pass
-    box_numbers = [x for x in range(len(boxes))]
+    some = [x for x in range(len(boxes))]
     # print(some)
     unlocked_boxes[0] = 'unlocked'
     for i in range(len(boxes) - 1):
+        # if i in opened_boxes:
         for k in range(len(boxes[i])):
-            if boxes[i][k] in box_numbers:
+            if boxes[i][k] in some:
                 unlocked_boxes[boxes[i][k]] = 'unlocked'
 
     opened = unlocked_boxes.values()
+    print(unlocked_boxes)
+    # print(opened)
     if len(opened) != len(boxes):
         return False
     return True
+
+
+boxes: List[list] = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
