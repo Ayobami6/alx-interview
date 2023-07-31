ff6f90b1da094ed4c3352a3da32ab77e17fa6818
Ayobami <ayobamidele006@gmail.com>
2023-07-31 11:02:56 +0100

    refactor lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,18 +8,15 @@ def canUnlockAll(boxes: List[list]) -> bool:
         boxes[empty_index] = [0]
     except Exception:
         pass
-    some = [x for x in range(len(boxes))]
+    box_numbers = [x for x in range(len(boxes))]
     # print(some)
     unlocked_boxes[0] = 'unlocked'
     for i in range(len(boxes) - 1):
-        # if i in opened_boxes:
         for k in range(len(boxes[i])):
-            if boxes[i][k] in some:
+            if boxes[i][k] in box_numbers:
                 unlocked_boxes[boxes[i][k]] = 'unlocked'
 
     opened = unlocked_boxes.values()
-    print(unlocked_boxes)
-    # print(opened)
     if len(opened) != len(boxes):
         return False
     return True
